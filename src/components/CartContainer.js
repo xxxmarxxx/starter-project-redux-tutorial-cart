fd6c4abb001f3e82cd042c784f81753267ce26d8
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2021-03-01 16:58:22 +0100

    import connect add func mapState and connect

--- src/components/CartContainer.js
+++ src/components/CartContainer.js
@@ -1,6 +1,7 @@
 import React from "react";
 import CartItem from "./CartItem";
-const CartContainer = ({ cart = [] }) => {
+import {connect} from 'react-redux';
+const CartContainer = ({ cart = [],total }) => {
   if (cart.length === 0) {
     return (
       <section className="cart">
@@ -29,7 +30,7 @@ const CartContainer = ({ cart = [] }) => {
         <hr />
         <div className="cart-total">
           <h4>
-            total <span>$0.00</span>
+            total <span>€{total}</span>
           </h4>
         </div>
         <button className="btn clear-btn">clear cart</button>
@@ -37,5 +38,9 @@ const CartContainer = ({ cart = [] }) => {
     </section>
   );
 };
+function mapStateToProps(store){
+  const {cart,total} = store;
 
-export default CartContainer;
+  return{cart,total}
+}
+export default connect(mapStateToProps)(CartContainer);
