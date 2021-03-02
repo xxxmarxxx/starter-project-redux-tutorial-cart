0b5ff4908f8ca80b236ace5625acaaae631c6eda
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2021-03-02 10:57:36 +0100

    change REMOVE

--- src/reducer.js
+++ src/reducer.js
@@ -1,29 +1,27 @@
-import { DECREASE, INCREASE, CLEAR_CART,REMOVE } from "./actions";
+import { DECREASE, INCREASE, CLEAR_CART, REMOVE } from "./actions";
 
 function reducer(state, action) {
-  
   if (action.type === CLEAR_CART) {
-      return { ...state, cart: [] };
-    }
-    if(action.type === DECREASE){
-      console.log('you decreased amount')
-    }
-    if(action.type === INCREASE){
-      console.log('you increase amount')
-    }
-    if(action.type === REMOVE){
-      console.log(action.id)
-    }
-    return state;
-
+    return { ...state, cart: [] };
   }
-  export default reducer;
+  if (action.type === DECREASE) {
+    console.log("you decreased amount");
+  }
+  if (action.type === INCREASE) {
+    console.log("you increase amount");
+  }
+  if (action.type === REMOVE) {
+    return { ...state, cart: state.cart.filter((cartItem)=>cartItem.id !== action.payload.id) };
+  }
+  return state;
+}
+export default reducer;
 
-  // ------ mozna tez switch zamiast if ------
+// ------ mozna tez switch zamiast if ------
 
-  // switch (action.type) {
-  //   case CLEAR_CART:
-  //     return { ...state, cart: [] };
-  //   default:
-  //     return state;
-  // }
+// switch (action.type) {
+//   case CLEAR_CART:
+//     return { ...state, cart: [] };
+//   default:
+//     return state;
+// }
