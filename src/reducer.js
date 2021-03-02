58b9b4194628a219545b15c140999fc76d0e962f
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2021-03-02 13:37:52 +0100

    add GET_TOTALS, if action.type

--- src/reducer.js
+++ src/reducer.js
@@ -1,4 +1,4 @@
-import { DECREASE, INCREASE, CLEAR_CART, REMOVE } from "./actions";
+import { DECREASE, INCREASE, CLEAR_CART, REMOVE, GET_TOTALS } from "./actions";
 
 function reducer(state, action) {
   if (action.type === CLEAR_CART) {
@@ -7,7 +7,9 @@ function reducer(state, action) {
   if (action.type === DECREASE) {
     let tempCart = [];
     if (action.payload.amount === 1) {
-      tempCart = state.cart.filter((cartItem) => cartItem.id !== action.payload.id)
+      tempCart = state.cart.filter(
+        (cartItem) => cartItem.id !== action.payload.id
+      );
       // console.log(`hey it's one`);
     } else {
       tempCart = state.cart.map((cartItem) => {
@@ -33,6 +35,23 @@ function reducer(state, action) {
       ...state,
       cart: state.cart.filter((cartItem) => cartItem.id !== action.payload.id),
     };
+  }
+  if (action.type === GET_TOTALS) {
+    let { total, amount } = state.cart.reduce(
+      (cartTotal, cartItem) => {
+        const { price, amount } = cartItem;
+        const itemTotal = price * amount;
+        cartTotal.total += itemTotal;
+        cartTotal.amount += amount;
+        return cartTotal;
+      },
+      {
+        total: 0,
+        amount: 0,
+      }
+    );
+    total = parseFloat(total.toFixed(2))
+    return { ...state, total, amount };
   }
   return state;
 }
