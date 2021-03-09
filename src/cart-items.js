aa2b74bbd2cc85ad9f8916e56b3729b48c7a5e5a
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2021-03-09 13:28:16 +0100

    import

--- src/cart-items.js
+++ src/cart-items.js
@@ -1,3 +1,6 @@
+import iphone12 from "../src/assets/iphone12.png";
+import iphone12Max from "../src/assets/iphone12Max.png";
+import {bild_1,bild_2} from './image'
 export default [
   {
     id: 1,
@@ -5,7 +8,7 @@ export default [
     price: 599.99,
     img:
       "https://res.cloudinary.com/diqqf3eq2/image/upload/v1583368215/phone-2_ohtt5s.png",
-    amount: 1
+    amount: 1,
   },
   {
     id: 2,
@@ -13,7 +16,7 @@ export default [
     price: 499.99,
     img:
       "https://res.cloudinary.com/diqqf3eq2/image/upload/v1583371867/phone-1_gvesln.png",
-    amount: 1
+    amount: 1,
   },
   {
     id: 3,
@@ -21,6 +24,34 @@ export default [
     price: 699.99,
     img:
       "https://res.cloudinary.com/diqqf3eq2/image/upload/v1583368224/phone-3_h2s6fo.png",
-    amount: 1
-  }
+    amount: 1,
+  },
+  {
+    id: 4,
+    title: "iphone 12",
+    price: 299.99,
+    img: iphone12,
+    amount: 1,
+  },
+  {
+    id: 5,
+    title: "iphone 12 pro max",
+    price: 1219.99,
+    img: iphone12Max,
+    amount: 1,
+  },
+  {
+    id: 6,
+    title: "iphone 12 pro max 128GB",
+    price: 1219,
+    img: bild_1,
+    amount: 1,
+  },
+  {
+    id: 7,
+    title: "iphone 12 pro max 512GB",
+    price: 1599,
+    img: bild_2,
+    amount: 1,
+  },
 ];
